ce86f426dce04a335ae6339ab12774c6ac740792
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2023-08-17 07:07:27 -0300

    fix: add folder to be searched

--- bin/.local/bin/tmux-fzfinderness.sh
+++ bin/.local/bin/tmux-fzfinderness.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dest_dir=$(find ~ ~/Documents/1.projects ~/Documents/2.areas ~/Documents/3.resources -type d -mindepth 1 -maxdepth 1 | fzf)
+dest_dir=$(find ~ ~/Documents/ ~/Documents/1.projects ~/Documents/2.areas ~/Documents/3.resources -type d -mindepth 1 -maxdepth 1 | fzf)
 session_name=$(basename "$dest_dir" | tr . _)
 
 if ! tmux has-session -t "$session_name" 2> /dev/null; then
